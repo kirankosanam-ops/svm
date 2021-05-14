db1b331fa772c0544c792462139c98acbc6f4d8c
kiran <kirankosanam1@gmail.com>
2021-05-14 19:23:44 +0530

    CV:-1.0

--- breast_cancer_code.py
+++ breast_cancer_code.py
@@ -31,19 +31,27 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-clf = SVC(kernel="rbf", random_state=0)
-clf.fit(X_train, y_train)
-label_pred = clf.predict(X_test)
+clf_sigmoid = SVC(kernel="sigmoid", random_state=0)  # sigmoid -> 92.1428571428571
+clf_sigmoid.fit(X_train, y_train)
+label_pred_sigmoid = clf_sigmoid.predict(X_test)
 
-cm = confusion_matrix(y_test, label_pred)
-score = clf.score(X_test, y_test)
-print(score * 100)
+clf_rbf = SVC(kernel="rbf", random_state=0)  # rbf -> 97.14285714285714
+clf_rbf.fit(X_train, y_train)
+label_pred_rbf = clf_rbf.predict(X_test)
 
-# custome input
-vals = [6, 2, 5, 3, 9, 4, 7, 2, 2]
+cm = confusion_matrix(label_pred_sigmoid, label_pred_rbf)
+score = clf_sigmoid.score(X_test, y_test)
+print('sigmoid', score * 100)
+
+score = clf_rbf.score(X_test, y_test)
+print('rbf', score * 100)
+
+# custom input
+vals1 = [6, 2, 5, 3, 9, 4, 7, 2, 2]  # 6, 2, 5, 3, 9, 4, 7, 2, 2,[4]
+vals = [4, 1, 1, 3, 2, 1, 3, 1, 1]  # 4,1,1,3,2,1,3,1,1,[2]
 a = sc.transform(np.array(vals).reshape(1, -1))
 # print(a)
 # 2 for Benign and 4 for Malignant
-cancer_type = ['', '', 'Benign', '', 'Malignant']
-cancer_pred = clf.predict(a)
+cancer_type = {2: 'Benign', 4: 'Malignant'}
+cancer_pred = clf_sigmoid.predict(a)
 print(cancer_type[cancer_pred[0]])
